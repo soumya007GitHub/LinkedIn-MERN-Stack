ef7f18f021393a277b3b56bb04f5a47863cc325f
soumya007GitHub <soumyatripathy2021@gmail.com>
2026-06-26 18:20:15 +0530

    Added code for login in frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <div className='w-full h-full min-h-screen flex items-center flex-col px-5 md:px-10 py-5 gap-5'>
+      <div className='w-full'>
+        <img src="/logo.svg" alt="Linkedin Logo" className='w-1/6 md:w-1/12' />
+      </div>
+      <div className='shadow-lg rounded md:rounded-lg w-[90%] md:w-1/2 lg:w-2/5 p-6 border border-gray-300 bg-white my-auto md:my-10'>
+        <h2 className='text-2xl font-semibold mb-6'>Sign in</h2>
+        
+        <form method="post" action="" className='flex flex-col gap-4'>
+          
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='email' className='text-sm font-medium text-gray-700'>Email</label>
+            <input 
+              type="email" 
+              name="email" 
+              id="email" 
+              className='border w-full border-gray-500 bg-transparent rounded-md p-2 hover:border-gray-800 focus:border-gray-800 hover-within:border-2 focus-within:border-2 outline-none' 
+              autoComplete="one-time-code"
+              required 
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor="password" className='text-sm font-medium text-gray-700'>Password</label>
+            <div className='flex justify-center items-center gap-1 border border-gray-500 pr-3 rounded-md hover:border-gray-800 focus:border-gray-800 hover-within:border-2 focus-within:border-2'>
+              <input 
+                type={showPassword ? "text" : "password"} 
+                name="password" 
+                id="password" 
+                className='w-full outline-none bg-transparent p-2' 
+                autoComplete="new-password"
+                required 
+              />
+              <button 
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className='text-gray-500 hover:text-gray-700 cursor-pointer focus:outline-none'
+              >
+                {showPassword ? <FaEye /> : <FaEyeSlash />}
+              </button>
+            </div>
+          </div>
+
+          <button 
+            type="submit"
+            className='flex justify-center items-center text-center w-full bg-sky-600 hover:bg-sky-700 text-white mt-4 p-3 rounded-3xl font-medium transition-colors'
+          >
+            Sign in
+          </button>
+          <p className='text-center'>New to Linkedin ? <span className='text-blue-500 hover:underline'
+          onClick={()=>navigate("/register")}
+          >Join now</span></p>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
